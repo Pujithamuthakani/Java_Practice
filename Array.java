0fa5a48e1146ebd3fb47a6c035b7f9afc12a9ec0
Pujitha Muthakani <92877508+Pujithamuthakani@users.noreply.github.com>
2022-04-20 14:53:03 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+class Array{
+  public static void main(String args[]){
+    //Declaring an Array
+    int arr[];
+    //Creating an Array
+    arr=new int[2];
+    //Initializing Array Elements
+    arr[0]=10;
+    arr[1]=20;
+    for(int var:arr){
+      System.out.println(var);
+    }
+  }
+}
